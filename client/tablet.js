b7d8b6a4a772b1e1143be6dde9a7bb308555d2bd
SMARTIUM <dev@smartium.io>
2019-10-17 15:17:13 -0300

    Funcionalidade do envio de OSC.

--- client/tablet.js
+++ client/tablet.js
@@ -1,2 +1,32 @@
 import '../imports/tablet.scss';
 import './tablet.html';
+
+Template.tablet.events({
+  'click button'(e) {
+    switch (e.target.id) {
+      case 'oscButton1':
+      Meteor.call('sendOsc', '/composition/columns/1/connect');
+      break;
+
+      case 'oscButton2':
+      Meteor.call('sendOsc', '/composition/columns/2/connect');
+      break;
+
+      case 'oscButton3':
+      Meteor.call('sendOsc', '/composition/columns/3/connect');
+      break;
+
+      case 'oscButton4':
+      Meteor.call('sendOsc', '/composition/columns/4/connect');
+      break;
+
+      case 'oscButton5':
+      Meteor.call('sendOsc', '/composition/columns/5/connect');
+      break;
+
+      case 'oscButton6':
+      Meteor.call('sendOsc', '/composition/columns/6/connect');
+      break;
+    }
+  }
+});
--- server/main.js
+++ server/main.js
@@ -1,5 +1,25 @@
 import { Meteor } from 'meteor/meteor';
+const dgram = require("dgram");
+const oscmsg = require("osc-msg");
+
+const SEND_PORT = 7000;
+const SEND_IP = '10.10.15.1';
 
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+Meteor.methods({
+  sendOsc(_message) {
+    const message = {
+      address: _message,
+      args: [
+        { type: "integer", value: 1 }
+      ]
+    };
+    const buffer = oscmsg.encode(message);
+    const socket = dgram.createSocket("udp4");
+    socket.send(buffer, 0, buffer.length, SEND_PORT, SEND_IP);
+    console.log(`Mensagem OSC "${message.address}" enviada para o IP ${SEND_IP} na porta ${SEND_PORT}`);
+  }
 });
